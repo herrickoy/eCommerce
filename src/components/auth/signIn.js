e5fa926f2ea1a7fb17c0ef090d2126a60bf9e94f
Koy Herrick <herrickoy@gmail.com>
2020-04-16 20:23:57 -0600

    Nav bar layout and sizing
    used grid and flex to make the Nav responsive and vreated sigIn and signOut divs

---
+++ src/components/auth/signIn.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+  render() {
+      return (
+          <div className="sign-in">
+              Sign Innn
+          </div>
+      )
+  }
+}
+
+export default SignIn;
---
+++ src/components/auth/signUp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+  render() {
+      return (
+          <div className="sign-up">
+              Sign Uppp
+          </div>
+      )
+  }
+}
+
+export default SignUp;

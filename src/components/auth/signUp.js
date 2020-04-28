7359910c35dcfe0337ae9b71f1248909fe05bf38
Koy Herrick <herrickoy@gmail.com>
2020-04-27 21:57:14 -0600

    up to signup form end styles

--- src/components/auth/signUp.js
+++ src/components/auth/signUp.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SignUpForm from "./signupForm";
+import PageTitle from "../pageTitle";
 
 class SignUp extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+        
+    }
 
   render() {
       return (
           <div className="sign-up">
-              Sign Uppp
+              <PageTitle className='sign-up__page-title' title='Register' />
+              <SignUpForm onSubmit={this.onSubmit} className='sign-up__form'/>
           </div>
-      )
-  }
+        )
+    }
 }
+
 
 export default SignUp;

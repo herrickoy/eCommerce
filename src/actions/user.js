a4b6149a7ac2c1342ffc0652bea808815d2d5d3c
Koy Herrick <herrickoy@gmail.com>
2020-05-02 15:12:47 -0600

    finished purchase history component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                amount: 8.02,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Koy Herrick',
+                    shippingAddress: '1234 West State'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40
+                amount: 19.40,
+                orderNumber: 'B0048248352',
+                orderDate: new Date(),
+                creditCard: '-0001',
+                user: {
+                    name: 'Gracie Herrick',
+                    shippingAddress: '12 skiptomyloo Street'
+                }
+            },
+            {
+                _id: 2,
+                amount: 12.72,
+                orderNumber: 'C0048248367',
+                orderDate: new Date(),
+                creditCard: '-0002',
+                user: {
+                    name: 'Lance Fred',
+                    shippingAddress: '78 n Street Road'
+                }
+            },
+            {
+                _id: 3,
+                amount: 28.61,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0003',
+                user: {
+                    name: 'Huy Nguyen',
+                    shippingAddress: '58 n Albany Way'
+                }
+            },
+            {
+                _id: 4,
+                amount: 3.46,
+                orderNumber: 'D0048248371',
+                orderDate: new Date(),
+                creditCard: '-0004',
+                user: {
+                    name: 'Gage Han',
+                    shippingAddress: '411 w 10 n'
+                }
+            },
+            {
+                _id: 5,
+                amount: 78.84,
+                orderNumber: 'E0048248385',
+                orderDate: new Date(),
+                creditCard: '-0005',
+                user: {
+                    name: 'Spencer Scholle',
+                    shippingAddress: '678 Easy Street'
+                }
+            },
+            {
+                _id: 6,
+                amount: 25.87,
+                orderNumber: 'F0048248399',
+                orderDate: new Date(),
+                creditCard: '-0006',
+                user: {
+                    name: 'Cait Brennan',
+                    shippingAddress: '24 West Street'
+                }
+            },
+            {
+                _id: 7,
+                amount: 11.27,
+                orderNumber: 'G0048248407',
+                orderDate: new Date(),
+                creditCard: '-0007',
+                user: {
+                    name: 'Arlo Herrick',
+                    shippingAddress: '234 East Street'
+                }
             }
         ]
     })
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -13,12 +13,14 @@ class PurchaseHistory extends Component {
       }
   }
 
-  render() {
-      return (
-          <div className="purchase-history">
-              <PageTitle className='purchase-history__page-title' title='Purchase History'/>
-              <Purchases className='purchase-history__purchases'/>
-              <PurchaseDetail className='purchase-history__detail'/>
+    render() {
+        return (
+            <div className="purchase-history">
+                <PageTitle className='purchase-history__page-title' title='Purchase History'/>
+                <div className='purchase-history__content'>
+                    <Purchases className='purchase-history__purchases'/>
+                    <PurchaseDetail className='purchase-history__detail'/>
+                </div>
               <div className='purchase-history__bottom-border'></div>
           </div>
       )

8459fe5561f8af921f0121b0be38396db3323f74
Koy Herrick <herrickoy@gmail.com>
2020-05-03 14:53:17 -0600

    built account info form basic layout

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInfoForm';
 
 class AccountInformation extends Component {
   constructor(props) {
@@ -9,10 +12,18 @@ class AccountInformation extends Component {
       }
   }
 
+    onSubmit = (fields) => {
+      console.log(fields);
+    
+    }
+
   render() {
       return (
-          <div>
-              account information
+          <div className='account-information'>
+              <PageTitle className='account-information' title='Account Information'/>
+              <AccountInformationForm onSubmit={this.onSubmit} className='account-information__form'/>
+              
+
           </div>
       )
   }
--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 function PurchaseDetailLabel ({className, title, value}) {
     return (
         <div className={`${className} purchase-detail-label`}>
-            <div className='purchase-detail__title'>{title}</div>
-            <div className='purchase-detail__value'>{value}</div>
+            <div className='purchase-detail-label__title'>{title}</div>
+            <div className='purchase-detail-label__value'>{value}</div>
         </div>
     )
 }
@@ -28,34 +28,37 @@ class PurchaseDetail extends Component {
   render() {
         const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
         const { name, shippingAddress } = user;
+        
 
         return (
             <div className={`${className} purchase-detail`}>
                 <PurchaseDetailLabel 
                 className='purchase-detail__order-number'
-                title='Order Number'
+                title='Order Number:'
                 value={orderNumber}
                 />
                 <PurchaseDetailLabel 
                 className='purchase-detail__order-date'
-                title='Order Date'
+                title='Order Date:'
                 value={orderDate}
                 />
                 <PurchaseDetailLabel 
-                className='purchase-detail__shipping'
-                title='Shipping Address'
-                value={`${name}\n${shippingAddress}`}
+                className='purchase-detail__shipping-address'
+                title='Shipping Address:'
+                value= {`${name} ${shippingAddress}`}
                 />
                 <PurchaseDetailLabel 
                 className='purchase-detail__total'
-                title='Total'
+                title='Total:'
                 value={total}
                 />
                 <PurchaseDetailLabel 
                 className='purchase-detail__credit-card'
-                title='Credit Card'
+                title='Credit Card:'
                 value={creditCard}
                 />
+                <a className='purchase-detail__track-shipment'>Track Shipment</a>
+                <a className='purchase-detail__print-receipt'>Print Receipt</a>
             </div>
         )
     }
